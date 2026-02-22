fbea84a684726293b4c4f8b701cdc5d37ffc101a
EmilianoCSantos <cactus8976@hotmail.com>
2026-02-23 00:36:54 +0100

    Fixat setting och program

--- Projekt B/ProjectB_GoodFriends-main/AppRazor/Program.cs
+++ Projekt B/ProjectB_GoodFriends-main/AppRazor/Program.cs
@@ -1,7 +1,42 @@
+using Configuration;
+using Configuration.Extensions;
+using Configuration.Options;
+using DbContext.Extensions;
+using DbRepos;
+using Encryption.Extensions;
+using Services;
+using Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// För Razor Pages
 builder.Services.AddRazorPages();
+
+// Secrets och configuration
+builder.Configuration.AddSecrets(builder.Environment);
+
+// Encryption, database, services
+builder.Services.AddEncryptions(builder.Configuration);
+builder.Services.AddJwtToken(builder.Configuration);
+builder.Services.AddDatabaseConnections(builder.Configuration);
+builder.Services.AddUserBasedDbContext();
+
+builder.Services.AddVersionInfo();
+builder.Services.AddEnvironmentInfo();
+builder.Services.AddInMemoryLogger();
+
+// Registrera DbRepos och Services
+builder.Services.AddScoped<AdminDbRepos>();
+builder.Services.AddScoped<FriendsDbRepos>();
+builder.Services.AddScoped<AddressesDbRepos>();
+builder.Services.AddScoped<PetsDbRepos>();
+builder.Services.AddScoped<QuotesDbRepos>();
+
+builder.Services.AddScoped<IAdminService, AdminServiceDb>();
+builder.Services.AddScoped<IFriendsService, FriendsServiceDb>();
+builder.Services.AddScoped<IAddressesService, AddressesServiceDb>();
+builder.Services.AddScoped<IPetsService, PetsServiceDb>();
+builder.Services.AddScoped<IQuotesService, QuotesServiceDb>();
 
 var app = builder.Build();
 
@@ -9,18 +44,13 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
+app.UseStaticFiles();
 app.UseRouting();
-
 app.UseAuthorization();
-
-app.MapStaticAssets();
-app.MapRazorPages()
-   .WithStaticAssets();
+app.MapRazorPages();
 
 app.Run();
